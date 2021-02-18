1c4f03f3b8f3c4ccd049aa97275211c0a054da9b
Diurno <Diurno@C8PC23.informatica.comercio>
2021-02-18 11:41:46 +0100

    Version 1

---
+++ src/crackmaster/CrackMaster.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crackmaster;
+
+/**
+ *
+ * @author Diurno
+ */
+public class CrackMaster {
+    private static final int MISHUEVOS = 2;
+    private int edadManuel = 18; 
+
+
+public int getEdadManuel()
+{
+    return edadManuel;
+}
+
+public int getMisHuevos()
+{
+    return MISHUEVOS;
+}
+}
