7d4c51d64b951ac2dc4a8bcebb33fd85bcc6b795
Diurno <Diurno@C8PC23.informatica.comercio>
2021-02-18 12:39:19 +0100

    Version 1

--- src/crackmaster/CrackMaster.java
+++ src/crackmaster/CrackMaster.java
@@ -13,20 +13,21 @@ public class CrackMaster {
     private static final int PUTO = 2;
     private int edadManuel = 18; 
     private String aids = "Github es un bro momento";
+    private boolean bryanEsUnMaster = true;
+    private float miAltura = 1.82;
 
+    public int getEdadManuel()
+    {
+        return edadManuel;
+    }
 
-public int getEdadManuel()
-{
-    return edadManuel;
-}
+    public int getPuto()
+    {
+        return PUTO;
+    }
 
-public int getPuto()
-{
-    return PUTO;
-}
-
-public String xd()
-{
-    return aids;
-}
+    public String xd()
+    {
+        return aids;
+    }
 }
