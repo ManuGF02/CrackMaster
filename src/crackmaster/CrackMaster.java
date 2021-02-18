1380dc7895a7ae6801bfe37b509485702fcde886
Diurno <Diurno@C8PC23.informatica.comercio>
2021-02-18 12:42:43 +0100

    Conflicto resuelto LOL

--- src/crackmaster/CrackMaster.java
+++ src/crackmaster/CrackMaster.java
@@ -10,23 +10,31 @@ package crackmaster;
  * @author Diurno
  */
 public class CrackMaster {
+
     private static final int ADS = 2;
-    private int edadManuel = 18; 
+    private int edadManuel = 18;
     private String add = "Github es un bro momento";
+    private String aids = "Github es un bro momento";
+    private boolean bryanEsUnMaster = true;
+    private float miAltura = 1.82;
 
+    public int getEdadManuel() {
+        return edadManuel;
+    }
 
-public int getEdadManuel()
-{
-    return edadManuel;
-}
+    public int getPuto() {
+        return PUTO;
+    }
 
-public int getPuto()
-{
-    return ADS;
-}
+    public int getPuto() {
+        return ADS;
+    }
 
-public String xd()
-{
-    return add;
-}
+    public String xd() {
+        return add;
+    }
+
+    public String xd() {
+        return aids;
+    }
 }
